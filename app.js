0d9b8e572d238b0cf6203ae53cf5a4326b506129
markjnkim <markjnkim@gmail.com>
2021-06-26 12:46:40 -0700

    add hike routes and template

--- app.js
+++ app.js
@@ -3,15 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var hike = require('./routes/hike');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.get('/hikes', hike.index);
+app.post('/add_hike', hike.add_hike);
 
 app.use(logger('dev'));
 app.use(express.json());
